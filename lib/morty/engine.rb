ae873f5bbc10e2178f51eea4e3e6cae1a2a0158b
Erik Peterson <thecompanygardener@gmail.com>
2026-04-15 11:45:11 -0400

    stop adding a migrate_path

--- lib/morty/engine.rb
+++ lib/morty/engine.rb
@@ -9,13 +9,5 @@ module Morty
 
   class Engine < ::Rails::Engine
     isolate_namespace Morty
-
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match?(root.to_s)
-        config.paths["db/migrate"].expanded.each do |path|
-          app.config.paths["db/migrate"] << path unless app.config.paths["db/migrate"].include?(path)
-        end
-      end
-    end
   end
 end
